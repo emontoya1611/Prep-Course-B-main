3b1a5371e00c5ae2a4e2d3d0b73ed82641580e8f
emontoya1611 <emontoya1620@gmail.com>
2022-09-19 22:03:23 -0500

    actualizacion semana 1

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,101 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    
+    // METODO "FOR"
+
+    /* var acumulador = 0;
+
+    for (var i = 1; i < 11; i++) {
+      acumulador += i;
+    }
+    return acumulador; */
+
+    // METODO "WHILE"
+
+    var i = 1;
+    var acumulador = 0;
+    while (i < 11) {
+      acumulador += i;
+      i++;
+    }
+    return acumulador
+    
+
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  /* var pares = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+    pares.push(array[i]);
+    }
+  }
+  return pares; */
+
+  // METODO FOR OF
+  var pares = [];
+
+  for (const elements of array) {
+
+    if (elements % 2 === 0) {
+    pares.push(elements);
+    }
+  }
+  return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  /* var elevados = [];
+  for (var i = 0; i < array.length; i++) {
+    elevados.push(Math.pow(array[i], 2));
+  }
+  return elevados; */
+
+  return array.map(function(valor){
+    return valor ** 2;
+  })
+
+  //o este metodo 
+
+  var resultado = array.map(function(valor){
+    return valor ** 2;
+  })
+  return resultado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+    var suma = array.reduce(function (acc, elemento) {
+    return acc + elemento;
+    });
+    return suma;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  /* var contador = 0;
+
+  while (num >= 1) {
+    ++contador;
+
+    num /= 10;
+  }
+
+  return contador; */
+
+  // METODO TOSTRING
+
+  return num.toString().length;
 }
   
   
